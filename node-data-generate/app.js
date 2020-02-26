53bdf56aa2286ebcd65096ce47bc825e82e438ea
ian <47257912+ianjamesx@users.noreply.github.com>
2020-02-26 15:07:26 -0500

    Add files via upload

---
+++ node-data-generate/app.js
@@ -0,0 +1,39 @@
+/*
+ldap - auto generate shit data
+
+install/run
+-mkdir node_modules
+-npm i install
+-node app
+*/
+
+var ldap = require('ldapjs');
+var faker = require('faker');
+
+var usercount = 10; //how many fake users to add
+
+var client = ldap.createClient({
+  url: 'ldap://127.0.0.1:1389' //put the url here
+});
+
+var i;
+for(i = 0; i < usercount; i++){
+
+  var fn = faker.name.firstName(),
+      ln = faker.name.lastName(),
+      email = faker.internet.email();
+
+  //console.log('adding entry: ' + fn + ' ' + ln + ' : ' + email);
+
+  var entry = {
+    cn: fn,
+    sn: ln,
+    email: [email],
+    objectclass: 'put_something_here'
+  };
+
+  client.add('cn=foo, o=example', entry, function(err){ //asyn in a for, bad, fix later
+    assert.ifError(err);
+  });
+
+}
