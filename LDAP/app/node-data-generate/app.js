05b27ad3aa986e3101484cc4aaf1a56699f061ae
Bair12 <user@localhost.localdomain>
2020-03-25 14:42:55 -0400

    working ldap with node

---
+++ LDAP/app/node-data-generate/app.js
@@ -0,0 +1,50 @@
+/*
+ldap - auto generate shit data
+
+install/run
+-mkdir node_modules
+-npm i install
+-node app
+*/
+//var web_url=process.env.RUNNABLE_CONTAINER_ID;
+var ldap = require('ldapjs');
+var faker = require('faker');
+//console.log(web_url);
+var usercount = 10; //how many fake users to add
+
+var client = ldap.createClient({
+	url: 'ldap://user_openldap_1' //put the url here
+	//url: 'ldap://172.18.0.2' //put the url here
+	//url: 'openldap' //put the url here
+});
+
+var i;
+for(i = 0; i < usercount; i++){
+
+  var fn = faker.name.firstName(),
+      ln = faker.name.lastName(),
+      email = faker.internet.email();
+
+  //console.log('adding entry: ' + fn + ' ' + ln + ' : ' + email);
+
+  var entry = {
+    cn: fn,
+    sn: ln,
+    email: [email],
+    objectclass: 'put_something_here'
+  };
+  
+  /*
+  
+  adding api - add(dn, entry, controls, callback)
+  only thing to change here is dn
+  
+  */
+
+  client.add('cn=foo, o=example', entry, function(err){ //async in a for, bad, fix later
+    if(err){
+      console.log(err);
+    }
+  });
+
+}
