d60fcd3367d45a37a0e5f9eb99bb7dec5df6f0c7
bair12 <user@localhost.localdomain>
2020-04-01 15:21:41 -0400

    final working nodejs with openldap

--- LDAP/app/node-data-generate/app.js
+++ LDAP/app/node-data-generate/app.js
@@ -12,7 +12,7 @@ var faker = require('faker');
 var usercount = 1; //how many fake users to add
 
 var client = ldap.createClient({
-	url: 'ldap://user_openldap_1' //put the url here
+	url: 'ldap://openldap' //put the url here
 });
 
 client.bind('cn=admin,dc=linuxlab,dc=salisbury,dc=edu', 'password' ,function(err){
@@ -31,20 +31,20 @@ for(i = 0; i < usercount; i++){
   /*
   dn format: cn=faculty,ou=group,dc=linuxlab,dc=salisbury,dc=edu
   */
-
+  var array = ["top", "posixAccount", "inetOrgPerson"];
   var entry = {
-    cn: 'Dummy Account3',
-    //gidNumber: '100' ,
-    //homeDirectory: '/home/dummyaccount',
-    objectClass: 'top',
-    objectClass: 'posixAccount',
-    objectClass: 'inetOrgPerson',
-    sn: 'dummyaccount3',
-    uid: 'dummyaccount3'
-    //uidNumber: '1'
+    cn: 'Dummy Account6',
+    gidNumber: '100' ,
+    homeDirectory: "home/dummyaccount",
+    //objectClass: 'top',
+    //objectClass: 'posixAccount',
+    objectClass: array ,
+    sn: 'dummyaccount6',
+    uid: 'dummyaccount6',
+    uidNumber: '1'
   };
   
-  var dn = 'uid=dummyaccount3,ou=people,dc=linuxlab,dc=salisbury,dc=edu'
+  var dn = 'uid=dummyaccount6,ou=people,dc=linuxlab,dc=salisbury,dc=edu'
   
   /*
   
