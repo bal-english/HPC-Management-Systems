2ed57e21c6b1d2cee835379fc4ce34f42ad96e71
Grace Burton <gburton5@gulls.salisbury.edu>
2020-04-27 09:29:15 -0400

    EJS templating

--- cms-db/controllers/index.js
+++ cms-db/controllers/index.js
@@ -2,46 +2,24 @@
 	var express = require('express');
 	var app = express();
 
-	app.set('views','./views');
-	// app.set('view engine', 'ejs');
-
-	var path = require('path');
-
-	app.use('/static', express.static(path.join(__dirname, '/views/images')));
-
-	app.use(express.static('dbconnect.js'));
-	//app.use(express.static(__dirname+"/views/css"));
-
-
-		//path prefix for serving static files. For example, if you want to provide a path prefix like '/static'
-		// app.use('/static', express.static('public'));
-		// Now whenever you need to include a file, for example, 
-		// a script file called main.js residing in your public directory, use the following script tag −
-
-		// <script src = "/static/main.js" />
-
-		//********This technique can come in handy when providing multiple directories as static files. 
-		//		  These prefixes can help distinguish between multiple directories.
+	app.set('view engine','ejs');
 
 	app.get('/', function(req, res) {
-    	res.sendFile(path.join(__dirname + '/views/home.html'));
+		res.render('pages/home');
 	});
 
-	app.get('/blog', function(req, res){
-    	res.sendFile(path.join(__dirname + '/views/bloghome.html'));
-	});
+	// app.get('/blog', function(req, res){
+	// 	res.render('views/bloghome');
+	// });
 
-	app.get('/admin', function(req, res){
-	    	res.sendFile(path.join(__dirname + '/views/adminhome.html'));
-		});
+	// app.get('/admin', function(req, res){
+	// 	res.render('views/admin');
+	// 	});
 
-	app.get('/ticket', function(req, res){
-	    	res.sendFile(path.join(__dirname + '/views/tickethome.html'));
-	});
 
-		app.get('/ticket/create', function(req, res){
-    		res.sendFile(path.join(__dirname + '/views/ticketcreation.html'));
-		});
+	// app.get('/ticket/create', function(req, res){
+	// 	res.render('views/ticketcreate');
+	// });
 
 
 
@@ -75,43 +53,34 @@
 
 // ````````````
 
-		//id can only be 5 digits from 0-9
-		//for blog posts
-		//Should connect to Postgres & return data
-	app.get('/blog/:id([0-9]{5})', function(req, res){
-	   res.send("TODO: Blog individual page goes here");
-	   res.send('id: ' + req.params.id);
-	});
-
 
 
 	//**** BLOG GROUPING ****
 
-	//any page the user searches for manually in URL with "blog" literal will show main page (for now)
-	app.get(/blog/, function (req, res) {
-  		res.sendFile(path.join(__dirname + '/views/bloghome.html'));
-	});
-
-
+	// //any page the user searches for manually in URL with "blog" literal will show main page (for now)
+	// app.get(/blog/, function (req, res) {
+ //  		res.sendFile(path.join(__dirname + '/views/bloghome.html'));
+	// });
 
 		//id can only be 5 digits from 0-9
-		//for tickets
+		//for blog posts
 		//Should connect to Postgres & return data
-	app.get('/ticket/:id([0-9]{5})', function(req, res){
-	   res.send("TODO: Ticket individual page goes here");
-	   res.send('id: ' + req.params.id);
-	});
-
+	// app.get('/blog/:id([0-9]{5})', function(req, res){
+	//    res.send("TODO: Blog individual page goes here");
+	//    res.send('id: ' + req.params.id);
+	// });
 
 
 
 //**** TICKET GROUPING ****
 
-	//any page the user searches for manually in URL with "blog" literal will show main page (for now)
-	app.get(/blog/, function (req, res) {
-  		res.sendFile(path.join(__dirname + '/views/bloghome.html'));
-	});
-
+	// 	//id can only be 5 digits from 0-9
+	// 	//for tickets
+	// 	//Should connect to Postgres & return data
+	// app.get('/ticket/:id([0-9]{5})', function(req, res){
+	//    res.send("TODO: Ticket individual page goes here");
+	//    res.send('id: ' + req.params.id);
+	// });
 
 
 //***** OTHER ROUTES *****
