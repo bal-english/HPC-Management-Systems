3618751eca1d60233fbae8cd595050704a95399d
Michael Mandulak <budroan@aol.com>
2020-05-01 16:43:46 -0400

    Working add user through skeleton web page

--- LDAP/app/src/index.ts
+++ LDAP/app/src/index.ts
@@ -1,22 +1,16 @@
-import * as express from 'express';
+import express from 'express';
 import * as bodyparser from 'body-parser';
+import { Request, Response } from 'express';
+import { User } from "./users";
 
 const app = express();
 app.set('view engine', 'ejs');
 app.engine('ejs', require('ejs').renderFile);
 
-/*
-let template = ejs.compile(str, options); template(data); // => Rendered HTML string
-  ejs.render(str, data, options); // => Rendered HTML string
-  ejs.renderFile(filename, data, options, function(err, str){ 
-  // str => Rendered HTML string 
-});
-*/
-
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-app.get('/', (request, response) => {
+app.get('/', (request:Request, response:Response) => {
   // response.sendFile(__dirname + '/views/pages/index.html');
   response.render('pages/index', {
     test: "Welcome Back... Richard"
@@ -31,24 +25,39 @@ TODO:
   -bootstrap
 */
 
-app.get('/api/user/:id', (request, response) => {
+app.get('/api/user/:id', (request:Request, response:Response) => {
 
 });
 
-app.post('/api/user/create', (request, response) => {
+app.post('/api/user/create', async (request:Request, response:Response) => {
   console.log(request.body);
-  let email = request.body.email, cn = request.body.cn;
+  const email:string = request.body.email;
+  const cn:string = request.body.cn;
+  try {
+    const res = await User.createUser(cn, email);
+    const res1 = await res.save();
+    response.send({
+      success: res1,
+    });
+  }
+  catch (err) {
+    response.send({
+      error: err.toString(),
+    });
+  }
+  // .then((res:any)=> {return User.loadUser("uid=wwolf1,ou=people,dc=linuxlab,dc=salisbury,dc=edu")})
+
   response.send({
     error: 'this is an error'
   });
 });
 
-app.post('/api/user/modify', (request, response) => {
+app.post('/api/user/modify', (request:Request, response:Response) => {
   console.log(request.body);
 });
 
-app.post('/api/user/delete', (request, response) => {
+app.post('/api/user/delete',(request:Request, response:Response) => {
   console.log(request.body);
 });
 
-app.listen(8080);
+app.listen(80, 'node');
