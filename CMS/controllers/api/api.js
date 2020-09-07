324a06060146cfa9f5b633e7cdf3da9e83064e20
bal-english <brendan@inkwright.net>
2020-09-07 17:24:04 -0400

    Query Additions and Typo Fixes

--- CMS/controllers/api/api.js
+++ CMS/controllers/api/api.js
@@ -17,9 +17,13 @@ router.post('/users', db.createUser)
 router.put('/users/:id', db.updateUser)
 router.delete('/users/:id', db.deleteUser)
 
+router.get('/groups/user', db.getUsergroups)
+router.get('/groups/user/:id', db.getUsergroupById)
 router.post('/groups/user', db.createUsergroup)
 router.put('/groups/user', db.updateUsergroup)
 
+router.get('/groups/blog', db.getBloggroups)
+router.get('/groups/blog/:id', db.getBloggroupById)
 router.post('/groups/blog', db.createBloggroup)
 router.put('/groups/blog', db.updateBloggroup)
 
@@ -33,7 +37,8 @@ router.post('/ticket', db.createTicket)
 router.get('/tickets', db.getTickets)
 router.get('/ticket/:id', db.getTicketById)
 
-
+router.get('/permission', db.getPermissions)
+router.get('/permission/:id', db.getPermissionById)
 
 module.exports = {
 	router
