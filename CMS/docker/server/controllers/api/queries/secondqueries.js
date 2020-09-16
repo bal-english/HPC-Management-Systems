3c1afe1250edf375f1dd972100fb1ff0c824f87b
root <root@DESKTOP-MAIMUPK.localdomain>
2020-09-16 16:25:19 -0400

    Starting secondary API

--- CMS/docker/server/controllers/api/queries/secondqueries.js
+++ CMS/docker/server/controllers/api/queries/secondqueries.js
@@ -1,12 +1,25 @@
 const Pool = require('pg').Pool;
 const pool = new Pool();
 
-const getBlogByCategory = (req, res) => {
-	const category = req.params.category;
-	pool.query('SELECT * FROM \"blog\" WHERE category == $category', (error, results) => {
+const getBlogByGroup = (req, res) => {
+	const group = req.params.groupId;
+	pool.query('SELECT * FROM \"blog\" WHERE category == $group', (error, results) => {
 		if(error){
 			throw error;
 		}
 		res.status(200).json(results.rows);
 	});
 }
+
+const getBlogById = (req, res) => {
+	const id = parseInt(req.params.id);
+	
+	pool.query('SELECT * FROM \"\" WHERE id = $1', [id], (error, results) => {
+		if(error){
+		  throw error;
+		}
+		res.status(200).send(results.rows);
+	})
+}
+
+
