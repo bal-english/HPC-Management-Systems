f128ab63ed906b91e1f77d5d748c445a9e316bef
Dan Weitman <danweitman57@gmail.com>
2020-12-03 16:43:01 -0500

    Functional password reset with modal

--- LDAP/app/src/authentication.ts
+++ LDAP/app/src/authentication.ts
@@ -1,57 +1,57 @@
 import paseto from 'paseto'
 import crypto from 'crypto'
-import {User }from './users'
+import { User } from './users'
 import { Obfuscation } from './Obfuscation'
 import { response } from 'express'
 import { isNamedExportBindings } from 'typescript'
-const {V2} = paseto
+const { V2 } = paseto
 // lambda function fix
 
-const authenticateUser = async (req:any, res:any, next:any) => { // asymmetric key
-// console.log("In authUSER");
-   if (req.body.password){
-    req.body.password = new Obfuscation(req.body.password);
+const authenticateUser = async (req: any, res: any, next: any) => { // asymmetric key
+   // console.log("In authUSER");
+   if (req.body.password) {
+      req.body.password = new Obfuscation(req.body.password);
+   }
+   try {
+      console.log("COOKIE" + req.cookies)
+      const key = req.app.get('key');
+      if (req.cookies == null || req.cookies.token == null) {
+         res.redirect("/");
+      } else {
+         const token = req.cookies.token;
+         const { V2: { verify, decrypt } } = paseto
+         const payload: any = await verify(token, key);
+         console.log("Not junk " + JSON.stringify(payload));
+         return next();
+      }
+   } catch (error) {
+      if (error)
+         res.redirect("/");
+   }
 }
-try {
-console.log("COOKIE" + req.cookies)
-const key = req.app.get('key');
- if (req.cookies == null || req.cookies.token == null) {
-    res.redirect("/");
- }else{
-    const token = req.cookies.token;
-    const {V2: { verify, decrypt } } = paseto
-    const payload:any = await verify(token ,key);
-    console.log("Not junk " + JSON.stringify (payload));
-    return next();
- }
-}catch(error){
-   if(error)
-   res.redirect("/");
-}
-}
-const checkPermissions = (pageRoles:string[]) => {
+const checkPermissions = (pageRoles: string[]) => {
 
-   return async (req:any, res:any, next:any ) => {
+   return async (req: any, res: any, next: any) => {
       try {
          const key = req.app.get('key');
          const token = req.cookies.token;
-         const {V2: { verify, decrypt } } = paseto
-         const payload:any = await verify(token ,key);
+         const { V2: { verify, decrypt } } = paseto
+         const payload: any = await verify(token, key);
          const result = pageRoles.every(role => payload.roles.includes(role))
          console.log("User perm" + payload.roles)
          console.log("Page perm" + pageRoles)
          console.log("Result" + result)
-         if (!result){
+         if (!result) {
             res.send("Error: No Permission to access page!")
             // res.redirect("/");
          }
          return result
 
       }
-      catch (error){
+      catch (error) {
          if (error)
-         res.redirect("/");
+            res.redirect("/");
       }
    }
 }
-export {authenticateUser, checkPermissions};
+export { authenticateUser, checkPermissions };
--- LDAP/app/src/index.ts
+++ LDAP/app/src/index.ts
@@ -1,10 +1,9 @@
-/*Working on the reset password
-TODO
-<uo>
-  - Reroute to page to enter token
-  - Generate token
-  - Make a page to enter the token
-</uo>
+/*
+  Fix close & X button
+  Implement pw reset
+  thats it (probably)
+
+
 */
 import * as bodyparser from 'body-parser';
 import express from 'express';
@@ -27,19 +26,19 @@ const auth = require('./authentication');
 
 // TESTING
 User.createUserFromEmail("William Wolf", "wwolf1@gulls.salisbury.edu")
-.then(async (res:User)=>{
-  return res.save();
-})
-.then(async (res:User)=>{
-  await res.setUserPassword("Test");
-})
+  .then(async (res: User) => {
+    return res.save();
+  })
+  .then(async (res: User) => {
+    await res.setUserPassword("Test");
+  })
 // TESTING
 
-const {V2} = paseto
+const { V2 } = paseto
 const app = express();
 (async () => {
-const key = await V2.generateKey('public')
-app.set('key', key);
+  const key = await V2.generateKey('public')
+  app.set('key', key);
 })()
 app.use(cookieParser());
 app.set('view engine', 'ejs');
@@ -52,29 +51,30 @@ app.use(bodyparser.json());
   const key = await createPrivateKey(Buffer.from(process.env.AUTH_KEY, 'base64'));
   app.set('workerKey', key);
 
-// Generates static auth token
-// TODO
-//  - Middleware needs to be changed to admin role middleware
-//  - Ian has got it, talk to him
-  app.post('/admin/homeDirQueue/createToken', authenticateWorkerAuthToken, async (request:Request, response:Response) => {
-    const token = await V2.sign({subject: 'auth_token', issuer: 'homedirqueue'}, app.get('workerKey'), {subject: 'auth_token', issuer: 'homedirqueue'});
-    response.send({'token': token});
+  // Generates static auth token
+  // TODO
+  //  - Middleware needs to be changed to admin role middleware
+  //  - Ian has got it, talk to him
+  app.post('/admin/homeDirQueue/createToken', authenticateWorkerAuthToken, async (request: Request, response: Response) => {
+    const token = await V2.sign({ subject: 'auth_token', issuer: 'homedirqueue' }, app.get('workerKey'), { subject: 'auth_token', issuer: 'homedirqueue' });
+    response.send({ 'token': token });
 
   });
+
   // Transform an auth token into a session token
-  app.post('/api/homeDirQueue/session/renew_token', authenticateWorkerAuthToken, async (request:Request, response:Response) => {
-    const token = await V2.sign({subject: 'session_token', issuer: 'homedirqueue'}, app.get('workerKey'), {subject: 'session_token', issuer: 'homedirqueue'});
-    response.send({'token': token});
+  app.post('/api/homeDirQueue/session/renew_token', authenticateWorkerAuthToken, async (request: Request, response: Response) => {
+    const token = await V2.sign({ subject: 'session_token', issuer: 'homedirqueue' }, app.get('workerKey'), { subject: 'session_token', issuer: 'homedirqueue' });
+    response.send({ 'token': token });
   });
 
-  app.get('/admin/tokenTest', async (request:Request, response:Response) => {
+  app.get('/admin/tokenTest', async (request: Request, response: Response) => {
     response.sendFile('/app/views/pages/tokenTest.html');
   })
 
-  app.post('/api/homeDirQueue/query', authenticateWorkerSessionToken, async (request:Request, response:Response) => {
-    await new Promise<any>(async ()=>{
+  app.post('/api/homeDirQueue/query', authenticateWorkerSessionToken, async (request: Request, response: Response) => {
+    await new Promise<any>(async () => {
       const hdQueue = await UmsQueue.getQueue();
-      if(hdQueue.length === 0){
+      if (hdQueue.length === 0) {
         response.send({
           empty: true
         });
@@ -87,140 +87,199 @@ app.use(bodyparser.json());
         uidNum: currUser.uidNumber.toNumber()
       });
     })
-    .catch((err:any)=>{
-      console.log("ERROR: Tried to retrieve the home directory creation queue (" + request.originalUrl + ")");
-      console.log(err);
-      console.log(JSON.stringify(request));
-      response.send({
-        error: true,
-      })
-    });
+      .catch((err: any) => {
+        console.log("ERROR: Tried to retrieve the home directory creation queue (" + request.originalUrl + ")");
+        console.log(err);
+        console.log(JSON.stringify(request));
+        response.send({
+          error: true,
+        })
+      });
   });
 
-  app.post('/api/homeDirQueue/delete', authenticateWorkerSessionToken, async (request:Request, response:Response) => {
+  app.post('/api/homeDirQueue/delete', authenticateWorkerSessionToken, async (request: Request, response: Response) => {
     const dn = request.body.dn;
     const currUser = await User.loadUser(dn);
     await UmsQueue.removeByDn(currUser)
-    .catch((err:any)=>
-    {
-      console.log("ERROR: Tried to remove a user (" + dn + ") from the home directory creation queue (" + request.originalUrl + ")");
-      console.log(err);
-      console.log(JSON.stringify(request));
-      response.send({
-        error: true,
-      });
-    })
+      .catch((err: any) => {
+        console.log("ERROR: Tried to remove a user (" + dn + ") from the home directory creation queue (" + request.originalUrl + ")");
+        console.log(err);
+        console.log(JSON.stringify(request));
+        response.send({
+          error: true,
+        });
+      })
   });
 
 })()
+const resetPWfunction = (modalToggle: boolean) => {
+  if (!modalToggle) {
+    return async (request: Request, response: Response) => {
+      const email = request.body.email;
+      try {
+        if (!validateUserEmail(email)) {
+          throw new Error("Invalid university email entered");
+        }
+        const tempEmail = request.body.email.split("@");
+        const tempUID = "uid=" + tempEmail[0] + ",ou=people,dc=linuxlab,dc=salisbury,dc=edu";
+        const res = await User.loadUser(tempUID);
+        const token = await V2.sign({issuer: request.body.email}, app.get('key'), {issuer: request.body.email});
+        console.log(token);
+        // uhh note to richard email them the token
+        response.send({
+          modalToggle,
+          email: request.body.email
+        });
+      } catch (error) {
+        response.send({
+          error: error.toString()
+        });
 
-app.post('/api/user/resetPW', async (request:Request, response:Response) => {
-  const email = request.body.email;
-  
-  try {
-    if (!validateUserEmail(email))
-      throw new Error("Invaid university email entered");
-    const tempEmail = request.body.email.split("@");
-    const tempUID = "uid=" + tempEmail[0] + ",ou=people,dc=linuxlab,dc=salisbury,dc=edu"
-    let res = await User.loadUser(tempUID);
-  } catch(error){
+      }
+    }
+  } else {
+    return async (request1: Request, response1: Response) => {
+      if (request1.body.state === 'token') {
 
+        try {
+          const tokenPW = request1.body.token;
+          const keyPW = request1.app.get('key');
+          const { V2: { verify, decrypt } } = paseto;
+          const payload: any = await verify(tokenPW, keyPW, { maxTokenAge: '15m', issuer: request1.body.email });
+          response1.send({
+
+          });
+        } catch (error) {
+          response1.send({
+            error: "ERROR: Invalid token. Please try again."
+          });
+        }
+      } else if (request1.body.state === 'reset') {
+        try {
+          const payload = request1.body.token;
+          const tempEmail = payload.issuer.split("@");
+          const tempDN = "uid=" + tempEmail[0] + ",ou=people,dc=linuxlab,dc=salisbury,dc=edu"
+          let res = await User.loadUser(tempDN);
+          if (validateUserPass(request1.body.password)) {
+            res = await res.setUserPassword(request1.body.password.getPass());
+            response1.send({
+
+            })
+          } else {
+            response1.send({
+              error: "Password Error: must be composed of either letters, numbers or symbols <br> and at least 4 characters"
+            });
+          }
+        } catch (error) {
+
+          console.log("ERROR: Error when trying to reset password " + error.toString());
+          response1.send({
+            error: 'Unexpected error: ' + error.toString()
+          });
+        }
+      }
+    }
   }
-  
+}
 
-});
+app.post('/api/user/resetPW', resetPWfunction(false));
 
+app.post('/api/user/verifyPWToken', resetPWfunction(true));
 
-app.get('/', (request:Request, response:Response) => {
+app.post('/api/user/changePW', async (req: Request, res: Response, next: any) => {
+
+  if (req.body.password) {
+    req.body.password = new Obfuscation(req.body.password);
+  }
+  try {
+    const key = req.app.get('key');
+    console.log(req.body);
+    console.log("Squidward Tennisballs");
+    if (req.body == null || req.body.token == null) {
+       throw new Error('Malformed request');
+    } else {
+       const token = req.body.token;
+       const { V2: { verify, decrypt } } = paseto
+       const payload: any = await verify(token, key);
+       req.body.token = payload;
+       return next();
+    }
+ } catch (error) {
+    if (error) {
+       console.log("ERROR: Error when attempting to reset password " + error.toString());
+       res.send({
+        error: 'Unexpected error: ' + error.toString()
+      })
+    }
+ }
+
+},resetPWfunction(true));
+
+app.get('/', (request: Request, response: Response) => {
   response.render('pages/login', {
     test: "HPCL UMS Test"
   });
 });
 
-app.get('/dashboard', [auth.authenticateUser, auth.checkPermissions(["admin", "faculty"])], (request:Request, response:Response) => {
+app.get('/dashboard', [auth.authenticateUser, auth.checkPermissions(["admin", "faculty"])], (request: Request, response: Response) => {
   response.render('pages/dashboard', {
     test: "HPCL UMS Test2"
   });
 });
 
-app.get('/resetPW', /*auth.checkPermissions(["admin", "faculty", ])],*/ (request:Request, response:Response) => {
+app.get('/resetPW', /*auth.checkPermissions(["admin", "faculty", ])],*/(request: Request, response: Response) => {
   response.render('pages/resetPW', {
     test: "HPCL UMS Test2"
   });
 });
 
-app.get('/dashboard.js', (request:Request, response:Response) => {
-  response.sendFile( path.join(__dirname, '../views/pages/dashboard.js'))
+app.get('/dashboard.js', (request: Request, response: Response) => {
+  response.sendFile(path.join(__dirname, '../views/pages/dashboard.js'))
 });
 
 
 app.post('/api/user/create', [auth.authenticateUser, auth.checkPermissions(["admin"])], async (request: Request, response: Response) => {
-    const email: string = request.body.email;
-    const cn: string = request.body.cn;
-    const password: Obfuscation = request.body.password;
-    try {
-      if (!validateUserEmail(email))
-        throw new Error("Invaid university email entered");
-      if (!validateUserCN(cn))
-        throw new Error("Common name must be alphabetic and at least 3 characters");
-      if (!validateUserPass(password))
-        throw new Error("Password must be composed of either letters, numbers or symbols and at least 4 characters");
-
-      const res = await User.createUserFromEmail(cn, email);
-      const res1 = await res.save();
-      await UmsQueue.push(res);
-      response.send({
-        success: res1,
-      });
-    }
-    catch (err) {
-      response.send({
-        error: err.toString(),
-      });
-    }
-  });
-
-  app.post('/api/user/modify', [auth.authenticateUser, auth.checkPermissions(["admin", "faculty"])], async (request: Request, response: Response) => {
-    const dn: string = request.body.dn;
-    try {
-      if (!validateUserDN(dn))
-        throw new Error("Invaid User ID, needs to be composed of letters and numbers");
-      const tempUID = "uid=" + dn + ",ou=people,dc=linuxlab,dc=salisbury,dc=edu"
-      const userPassword: Obfuscation = request.body.password;
-      let res
-      try {
-        res = await User.loadUser(tempUID);
-      } catch (error) {
-        response.send({
-          error: "Could not load user with that User ID",
-        })
-        return
-      }
-      if (validateUserPass(userPassword)){
-        res = await res.setUserPassword(userPassword.getPass());
-      } else {
-        throw new Error("Password Error: must be composed of either letters, numbers or symbols <br> and at least 4 characters");
-      }
-      await res.save();
-      response.send({
-      });
-    }
-    catch (err) {
-      response.send({
-        error: err.toString(),
-      });
-    }
-  });
-
-
-app.post('/api/user/modifyOwn', [auth.authenticateUser, auth.checkPermissions(["admin", "faculty"])], async (request: Request, response: Response) => {
-  const userPassword: Obfuscation = request.body.password;
+  const email: string = request.body.email;
+  const cn: string = request.body.cn;
+  const password: Obfuscation = request.body.password;
   try {
-    const key = request.app.get('key');
-    const token = request.cookies.token;
-    const { V2: { verify, decrypt } } = paseto
-    const payload: any = await verify(token, key);
-    let res = await User.loadUser(payload.uid);
+    if (!validateUserEmail(email))
+      throw new Error("Invalid university email entered");
+    if (!validateUserCN(cn))
+      throw new Error("Common name must be alphabetic and at least 3 characters");
+    if (!validateUserPass(password))
+      throw new Error("Password must be composed of either letters, numbers or symbols and at least 4 characters");
+
+    const res = await User.createUserFromEmail(cn, email);
+    const res1 = await res.save();
+    await UmsQueue.push(res);
+    response.send({
+      success: res1,
+    });
+  }
+  catch (err) {
+    response.send({
+      error: err.toString(),
+    });
+  }
+});
+
+app.post('/api/user/modify', [auth.authenticateUser, auth.checkPermissions(["admin", "faculty"])], async (request: Request, response: Response) => {
+  const dn: string = request.body.dn;
+  try {
+    if (!validateUserDN(dn))
+      throw new Error("Invaid User ID, needs to be composed of letters and numbers");
+    const tempUID = "uid=" + dn + ",ou=people,dc=linuxlab,dc=salisbury,dc=edu"
+    const userPassword: Obfuscation = request.body.password;
+    let res
+    try {
+      res = await User.loadUser(tempUID);
+    } catch (error) {
+      response.send({
+        error: "Could not load user with that User ID",
+      })
+      return
+    }
     if (validateUserPass(userPassword)) {
       res = await res.setUserPassword(userPassword.getPass());
     } else {
@@ -236,13 +295,38 @@ app.post('/api/user/modifyOwn', [auth.authenticateUser, auth.checkPermissions(["
     });
   }
 });
+
+
+app.post('/api/user/modifyOwn', [auth.authenticateUser, auth.checkPermissions(["admin", "faculty"])], async (request: Request, response: Response) => {
+  const userPassword: Obfuscation = request.body.password;
+  try {
+    const key = request.app.get('key');
+    const token = request.cookies.token;
+    const { V2: { verify, decrypt } } = paseto
+    const payload: any = await verify(token, key);
+    let res = await User.loadUser(payload.uid);
+    if (validateUserPass(userPassword)) {
+      res = await res.setUserPassword(userPassword.getPass());
+    } else {
+      throw new Error("Password Error: must be composed of either letters, numbers or symbols and at least 4 characters");
+    }
+    await res.save();
+    response.send({
+    });
+  }
+  catch (err) {
+    response.send({
+      error: err.toString(),
+    });
+  }
+});
 // added authenticate user
-app.post('/api/user/delete', [auth.authenticateUser, auth.checkPermissions(["faculty", "admin"])], async (request: Request, response: Response, ) => {
+app.post('/api/user/delete', [auth.authenticateUser, auth.checkPermissions(["faculty", "admin"])], async (request: Request, response: Response,) => {
 
   if (!validateUserEmail(request.body.email))
     throw new Error("Invaid university email entered");
   const tempEmail = request.body.email.split("@");
-  const tempUID = "uid=" + tempEmail[0] + ",ou=people,dc=linuxlab,dc=salisbury,dc=edu"
+  const tempUID = "uid=" + tempEmail[0] + ",ou=people,dc=linuxlab,dc=salisbury,dc=edu";
   try {
     const user = await User.loadUser(tempUID)
     await user.disableUser();
@@ -263,7 +347,7 @@ app.post('/api/user/login', async (request: Request, response: Response) => {
   const tempUID = "uid=" + tempEmail[0] + ",ou=people,dc=linuxlab,dc=salisbury,dc=edu"
   let user: User
   try {
-    if (!validateUserEmail(email)){
+    if (!validateUserEmail(email)) {
       throw new Error();
     }
     if (password.getPass() === "!!")
